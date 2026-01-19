31bdc2ed0afcfa5608e0555e3697ca39fc9e6402
artur.cqueiroz <artur.cqueiroz@TIT0676456W11-1>
2026-01-19 17:13:14 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora.Test/CalculatorTest.cs
@@ -0,0 +1,32 @@
+﻿namespace Calculadora.Test
+{
+    public class CalculatorTest
+    {
+        // Teste unitário para o método Sum da classe Calculator
+        // Usando o framework XUnit.
+
+        // O atributo [Fact] indica que este é um método de teste unitário.
+        [Fact(DisplayName = "Dados numero validos, quando ha soma, retorna sucesso")]
+
+        // Métod convencional GWT
+        // G = Given: Dados/Entregue
+        // W = When: Quando
+        // T = Then: Então
+
+        public void DadosNumerosValidos_QuandoSoma_RetornaSucesso()
+        {
+            // Arrange: Configuração dos dados de entrada e do resultado esperado
+            int primeiroNumero = 5;
+            int segundoNumero = 10;
+            int resultadoEsperado = 15;
+
+            // Act: Ação, chamada do método que está sendo testado.
+            int resultadoAtual = Calculator.Sum(primeiroNumero, segundoNumero);
+
+            // Assert: Verificação se o resultado atual é ingual ao resultado esperado. 
+            Assert.Equal(resultadoEsperado, resultadoAtual);
+
+
+        }
+    }
+}
---
+++ Calculadora/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace Calculadora
+{
+    public class Calculator
+    {
+        // Método para efetuar a soma de dois números inteiros
+        public static int Sum(int primeiroNumero, int segundoNumero)
+        {
+            // Condição para verificar se os números são positivos
+            if (primeiroNumero <0 || segundoNumero <0)
+            {
+                return -1;
+            }
+            // Retorna a soma de ambos os valores inteiros
+            return primeiroNumero + segundoNumero;
+        }
+
+    }
+}
